ad6dfbd4520eb854ee94fc4762d910693e13642a
Benjamin <benjaminng.2017@sis.smu.edu.sg>
2020-09-05 19:02:38 +0800

    added 2 more api

--- mainsite/app/urls.py
+++ mainsite/app/urls.py
@@ -39,4 +39,6 @@ urlpatterns += [
 urlpatterns += [
     path('gettopgains', views.gettopgains, name='gettopgains'),
     path('getnews', views.getnews, name='getnews'),
+    path('getcompanysymbol', views.getcompanysymbol, name='getcompanysymbol'),
+    path('getcompanydata', views.getcompanydata, name='getcompanydata'),
 ]
--- mainsite/app/views.py
+++ mainsite/app/views.py
@@ -50,12 +50,30 @@ def gettopgains(request):
     return JsonResponse(get_top_gains().to_dict())
 
 import urllib.request, json 
-from .api_key import NEWS_API
+from .api_key import *
 def getnews(request):
     with urllib.request.urlopen("https://newsapi.org/v2/top-headlines?country=sg&category=business&apiKey=" + NEWS_API) as url:
         data = json.loads(url.read().decode())
     return JsonResponse(data)
 
+
+def getcompanysymbol(request):
+    if request.method == "GET":
+        keyword = request.GET.get("keyword")
+        with urllib.request.urlopen("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+ keyword +"&apikey=" + ALPHA_VANTAGE_API) as url:
+            data = json.loads(url.read().decode())
+    else:
+        data = {}
+    return JsonResponse(data)
+
+def getcompanydata(request):
+    if request.method == "GET":
+        symbol = request.GET.get("symbol")
+        with urllib.request.urlopen("https://www.alphavantage.co/query?function=OVERVIEW&symbol="+ symbol +"&apikey=" + ALPHA_VANTAGE_API) as url:
+            data = json.loads(url.read().decode())
+    else:
+        data = {}
+    return JsonResponse(data)
 
 # accounts views
 from django.contrib.auth.forms import UserCreationForm
