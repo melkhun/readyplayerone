5c3a06b72419140ea1847a0404856810048ec3ba
hfernelo <hfernelo@gmail.com>
2020-09-05 20:04:30 +0800

    added api

---
+++ frontend/src/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const base_url = 'http://localhost:8000/app'
+
+export async function getTopGains() {
+  const options = {
+    method: 'get',
+    url: base_url + '/gettopgains'
+  }
+  let response = await axios(options)
+  return response.data
+}
+
+export async function getNews() {
+  const options = {
+    method: 'get',
+    url: base_url + '/getnews'
+  }
+  let response = await axios(options)
+  return response.data
+}
+
+export async function getCompanySymbol(param) {
+  const options = {
+    method: 'get',
+    url: base_url + `/getcompanysymbol?keyword=${param}`
+  }
+  let response = await axios(options)
+  return response.data
+}
+
+export async function getCompanyData(param) {
+  const options = {
+    method: 'get',
+    url: base_url + `/getcompanydata?symbol=${param}`
+  }
+  let response = await axios(options)
+  return response.data
+}
