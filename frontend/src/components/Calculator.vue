9c0f55c82f2bded095dfe026faa7d7f55958c564
hfernelo <hfernelo@gmail.com>
2020-09-05 12:07:07 +0800

    sep frontend

---
+++ frontend/src/components/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="Calculator">
+
+<v-card flat>
+  <v-card-text>
+    <h1><b>calc zz</b></h1>
+  </v-card-text>
+</v-card>
+
+</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
