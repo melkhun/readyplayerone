8fa3bcd291247a20fec59b34331d90e3e26a246f
Hum Shi Jing <shijing.hum.2017@smu.edu.sg>
2020-09-06 12:55:46 +0800

    bug fix for models.py

--- mainsite/app/models.py
+++ mainsite/app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Portfolio(models.Model):
-    username = models.CharField(max_length=6, primary_key=True)
-    etfs = models.CharField()
-    futures = models.CharField()
-    bonds = models.CharField()
-    equities = models.CharField()
-    options = models.CharField()
+    username = models.CharField(max_length=100, primary_key=True)
+    etfs = models.CharField(max_length=10)
+    futures = models.CharField(max_length=10)
+    bonds = models.CharField(max_length=10)
+    equities = models.CharField(max_length=10)
+    options = models.CharField(max_length=10)
