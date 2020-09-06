37fbfa3e59682b587ba83b4a41b5d5b37b53a130
Benjamin <benjaminng.2017@sis.smu.edu.sg>
2020-09-06 11:00:26 +0800

    table still broken

--- frontend/src/components/QuickStart.vue
+++ frontend/src/components/QuickStart.vue
@@ -252,42 +252,18 @@ export default {
       },
       { text: "Calories", value: "calories" },
       { text: "Fat (g)", value: "fat" },
-      { text: "Carbs (g)", value: "carbs" },
-      { text: "Protein (g)", value: "protein" },
-      { text: "Iron (%)", value: "iron" },
+      // { text: "Carbs (g)", value: "carbs" },
+      // { text: "Protein (g)", value: "protein" },
+      // { text: "Iron (%)", value: "iron" },
     ],
     desserts: [
       {
         name: "Frozen Yogurt",
         calories: 159,
         fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: "1%",
-      },
-      {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: "1%",
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: "7%",
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: "8%",
+        // carbs: 24,
+        // protein: 4.0,
+        // iron: "1%",
       },
     ],
   }),
@@ -334,6 +310,17 @@ export default {
         var resp = await getTopGains()
         console.log(resp[1428])
         resp = JSON.parse(`${resp}`)
+        // this.desserts = 
+        // [
+        //   {
+        //     name: "Frozen Yogurt",
+        //     calories: 159,
+        //     fat: 6.0,
+        //     // carbs: 24,
+        //     // protein: 4.0,
+        //     // iron: "1%",
+        //   },
+        // ],
         console.log(typeof resp)
         
       } catch(e) {
