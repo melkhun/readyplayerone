c7135815d68279bb00ea8f702c5f620a838b2c76
melkhun <melvin.ong.2017@sis.smu.edu.sg>
2020-09-06 16:30:52 +0800

    Portfolio style

--- frontend/src/components/Portfolio.vue
+++ frontend/src/components/Portfolio.vue
@@ -7,66 +7,38 @@
           ><h1><b>Your Portfolio Overview</b></h1></span
         ><br />
 
-        To insert chart of user here
-
-        <div v-if="assets" style="text-align:left;">
-          Assets
-          <hr />
-          <table>
-            <tr>
-              <td></td>
-              <td>High</td>
-              <td>Low</td>
-              <td>Open</td>
-              <td>Close</td>
-            </tr>
-            <div v-for="(assets, index) in listofassets" :key="assets">
-              <tr>
-                <td>Assets {{ index + 1 }}</td>
-                <td v-for="value in assets" :key="value">
-                  {{ value }}
-                </td>
-              </tr>
-            </div>
-          </table>
+        <div>
+          <div class="grid">
+            <ChartBar />
+            <ChartDoughnut />
+            <ChartLine />
+          </div>
         </div>
 
-        <!--Donut-->
-        
-  <div>
-    <h1>Chart Demo</h1>
-
-    <div class="grid">
-      <ChartBar />
-      <ChartDoughnut />
-      <ChartLine />
-    </div>
-  </div>
+        <div class="header-style">
+          Here's a breakdown of your portfolio
+        </div>
 
         <div>
-          <h1>Chart Demo</h1>
           <div v-if="assets" style="text-align:left;">
-          <h2><b>Assets</b></h2>
-          <hr>
+            <h2><b>Assets</b></h2>
+            <hr />
+          </div>
+
+          <div>
+            <v-data-table
+              v-model="selected"
+              :headers="headers"
+              :items="tableContent"
+              :item-key="$symbol"
+              show-select
+              class="elevation-1"
+            >
+            </v-data-table>
+            <v-btn @click="deleteSelected">Delete</v-btn>
+          </div>
         </div>
-
-        <div>
-          <v-data-table
-            v-model="selected"
-            :headers="headers"
-            :items="tableContent"
-            :item-key="$symbol"
-            show-select
-            class="elevation-1"
-          >
-          </v-data-table>
-          <v-btn @click="deleteSelected">Delete</v-btn>
-        </div>
-
-
-      </div>
       </v-card-text>
-
 
       <!-- output -->
     </v-card>
@@ -74,7 +46,6 @@
 </template>
 
 <script>
-
 import ChartDoughnut from "@/components/chart-doughnut";
 // import { getCompanyData } from '../api'
 
@@ -95,28 +66,89 @@ export default {
     tableContent: [],
     items: [],
     selected: [],
-    selectedData: []
+    selectedData: [],
   }),
   components: {
     ChartDoughnut,
   },
   mounted() {
-    this.getItems()
-    for (var i=0; i<this.items.length; i++) {
-      this.getCompanyData(this.items[i])
+    this.getItems();
+    for (var i = 0; i < this.items.length; i++) {
+      this.getCompanyData(this.items[i]);
     }
   },
   methods: {
     getItems() {
-      this.items = ['IBM', 'GOOGL']
+      this.items = ["IBM", "GOOGL"];
       // call some api
     },
     async getCompanyData(symbol) {
       try {
         // var resp = await getCompanyData(symbol);
-        var resp = {"Symbol": "IBM", "AssetType": "Common Stock", "Name": "International Business Machines Corporation", "Description": "International Business Machines Corporation operates as an integrated solutions and services company worldwide. Its Cloud & Cognitive Software segment offers software for vertical and domain-specific solutions in health, financial services, and Internet of Things (IoT), weather, and security software and services application areas; and customer information control system and storage, and analytics and integration software solutions to support client mission critical on-premise workloads in banking, airline, and retail industries. It also offers middleware and data platform software, including Red Hat, which enables the operation of clients' hybrid multi-cloud environments; and Cloud Paks, WebSphere distributed, and analytics platform software, such as DB2 distributed, information integration, and enterprise content management, as well as IoT, Blockchain and AI/Watson platforms. The company's Global Business Services segment offers business consulting services; system integration, application management, maintenance, and support services for packaged software; finance, procurement, talent and engagement, and industry-specific business process outsourcing services; and IT infrastructure and platform services. Its Global Technology Services segment provides project, managed, outsourcing, and cloud-delivered services for enterprise IT infrastructure environments; and IT infrastructure support services. The company's Systems segment offers servers for businesses, cloud service providers, and scientific computing organizations; data storage products and solutions; and z/OS, an enterprise operating system, as well as Linux. Its Global Financing segment provides lease, installment payment, loan financing, short-term working capital financing, and remanufacturing and remarketing services. The company was formerly known as Computing-Tabulating-Recording Co. and changed its name to International Business Machines Corporation in 1924. The company was founded in 1911 and is headquartered in Armonk, New York.", "Exchange": "NYSE", "Currency": "USD", "Country": "USA", "Sector": "Technology", "Industry": "Information Technology Services", "Address": "One New Orchard Road, Armonk, NY, United States, 10504", "FullTimeEmployees": "352600", "FiscalYearEnd": "December", "LatestQuarter": "2020-06-30", "MarketCapitalization": "108917817344", "EBITDA": "15576999936", "PERatio": "14.5329", "PEGRatio": "8.1011", "BookValue": "23.076", "DividendPerShare": "6.52", "DividendYield": "0.0509", "EPS": "8.811", "RevenuePerShareTTM": "85.058", "ProfitMargin": "0.1043", "OperatingMarginTTM": "0.1185", "ReturnOnAssetsTTM": "0.0362", "ReturnOnEquityTTM": "0.4097", "RevenueTTM": "75499003904", "GrossProfitTTM": "36489000000", "DilutedEPSTTM": "8.811", "QuarterlyEarningsGrowthYOY": "-0.458", "QuarterlyRevenueGrowthYOY": "-0.054", "AnalystTargetPrice": "135.19", "TrailingPE": "14.5329", "ForwardPE": "10.4493", "PriceToSalesRatioTTM": "1.518", "PriceToBookRatio": "5.5547", "EVToRevenue": "2.2493", "EVToEBITDA": "11.2431", "Beta": "1.17", "52WeekHigh": "158.75", "52WeekLow": "90.56", "50DayMovingAverage": "125.1934", "200DayMovingAverage": "122.3291", "SharesOutstanding": "890579008", "SharesFloat": "889189445", "SharesShort": "21600483", "SharesShortPriorMonth": "23242369", "ShortRatio": "4.51", "ShortPercentOutstanding": "0.02", "ShortPercentFloat": "0.0243", "PercentInsiders": "0.108", "PercentInstitutions": "58.555", "ForwardAnnualDividendRate": "6.52", "ForwardAnnualDividendYield": "0.0509", "PayoutRatio": "0.7358", "DividendDate": "2020-09-10", "ExDividendDate": "2020-08-07", "LastSplitFactor": "2:1", "LastSplitDate": "1999-05-27"}
-        console.log(symbol)
-        
+        var resp = {
+          Symbol: "IBM",
+          AssetType: "Common Stock",
+          Name: "International Business Machines Corporation",
+          Description:
+            "International Business Machines Corporation operates as an integrated solutions and services company worldwide. Its Cloud & Cognitive Software segment offers software for vertical and domain-specific solutions in health, financial services, and Internet of Things (IoT), weather, and security software and services application areas; and customer information control system and storage, and analytics and integration software solutions to support client mission critical on-premise workloads in banking, airline, and retail industries. It also offers middleware and data platform software, including Red Hat, which enables the operation of clients' hybrid multi-cloud environments; and Cloud Paks, WebSphere distributed, and analytics platform software, such as DB2 distributed, information integration, and enterprise content management, as well as IoT, Blockchain and AI/Watson platforms. The company's Global Business Services segment offers business consulting services; system integration, application management, maintenance, and support services for packaged software; finance, procurement, talent and engagement, and industry-specific business process outsourcing services; and IT infrastructure and platform services. Its Global Technology Services segment provides project, managed, outsourcing, and cloud-delivered services for enterprise IT infrastructure environments; and IT infrastructure support services. The company's Systems segment offers servers for businesses, cloud service providers, and scientific computing organizations; data storage products and solutions; and z/OS, an enterprise operating system, as well as Linux. Its Global Financing segment provides lease, installment payment, loan financing, short-term working capital financing, and remanufacturing and remarketing services. The company was formerly known as Computing-Tabulating-Recording Co. and changed its name to International Business Machines Corporation in 1924. The company was founded in 1911 and is headquartered in Armonk, New York.",
+          Exchange: "NYSE",
+          Currency: "USD",
+          Country: "USA",
+          Sector: "Technology",
+          Industry: "Information Technology Services",
+          Address: "One New Orchard Road, Armonk, NY, United States, 10504",
+          FullTimeEmployees: "352600",
+          FiscalYearEnd: "December",
+          LatestQuarter: "2020-06-30",
+          MarketCapitalization: "108917817344",
+          EBITDA: "15576999936",
+          PERatio: "14.5329",
+          PEGRatio: "8.1011",
+          BookValue: "23.076",
+          DividendPerShare: "6.52",
+          DividendYield: "0.0509",
+          EPS: "8.811",
+          RevenuePerShareTTM: "85.058",
+          ProfitMargin: "0.1043",
+          OperatingMarginTTM: "0.1185",
+          ReturnOnAssetsTTM: "0.0362",
+          ReturnOnEquityTTM: "0.4097",
+          RevenueTTM: "75499003904",
+          GrossProfitTTM: "36489000000",
+          DilutedEPSTTM: "8.811",
+          QuarterlyEarningsGrowthYOY: "-0.458",
+          QuarterlyRevenueGrowthYOY: "-0.054",
+          AnalystTargetPrice: "135.19",
+          TrailingPE: "14.5329",
+          ForwardPE: "10.4493",
+          PriceToSalesRatioTTM: "1.518",
+          PriceToBookRatio: "5.5547",
+          EVToRevenue: "2.2493",
+          EVToEBITDA: "11.2431",
+          Beta: "1.17",
+          "52WeekHigh": "158.75",
+          "52WeekLow": "90.56",
+          "50DayMovingAverage": "125.1934",
+          "200DayMovingAverage": "122.3291",
+          SharesOutstanding: "890579008",
+          SharesFloat: "889189445",
+          SharesShort: "21600483",
+          SharesShortPriorMonth: "23242369",
+          ShortRatio: "4.51",
+          ShortPercentOutstanding: "0.02",
+          ShortPercentFloat: "0.0243",
+          PercentInsiders: "0.108",
+          PercentInstitutions: "58.555",
+          ForwardAnnualDividendRate: "6.52",
+          ForwardAnnualDividendYield: "0.0509",
+          PayoutRatio: "0.7358",
+          DividendDate: "2020-09-10",
+          ExDividendDate: "2020-08-07",
+          LastSplitFactor: "2:1",
+          LastSplitDate: "1999-05-27",
+        };
+        console.log(symbol);
+
         this.tableContent.push({
           symbol: resp["Symbol"],
           name: resp["Name"],
@@ -124,11 +156,11 @@ export default {
           "50DayMovingAverage": resp["50DayMovingAverage"],
           "52WeekHigh": resp["52WeekHigh"],
           "52WeekLow": resp["52WeekLow"],
-          "DividendPerShare": resp["DividendPerShare"],
-          "ProfitMargin": resp["ProfitMargin"],
+          DividendPerShare: resp["DividendPerShare"],
+          ProfitMargin: resp["ProfitMargin"],
           cap: resp["MarketCapitalization"],
           ratio: resp["PERatio"],
-        })
+        });
       } catch (e) {
         console.error(e);
       }
@@ -138,15 +170,21 @@ export default {
         const data = {
           category: this.selected[id]["type"],
           symbol: this.selected[id]["symbol"],
-        }
-        console.log(data)
+        };
+        console.log(data);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
+.header-style {
+  padding: 150 40 150 40;
+  font-size: 2em;
+  color:blue;
+}
+
 .button-style {
   padding: 80px 50px 20px 50px;
 }
