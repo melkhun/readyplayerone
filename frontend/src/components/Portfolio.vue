824e088bfdf24134d7b82d2aabae8e6a416b673d
melkhun <melvin.ong.2017@sis.smu.edu.sg>
2020-09-06 11:56:54 +0800

    Portfolio

--- frontend/src/components/Portfolio.vue
+++ frontend/src/components/Portfolio.vue
@@ -1,73 +1,85 @@
 <template>
-  <div class="Quickstart">
-    <v-card flat class="block-card">
-      <!-- input -->
-      <v-card-text class="text-center">
-        <span class="text-blue"
-          ><h1><b>Your Portfolio Overview</b></h1></span
-        ><br />
+  <div class="Portfolio ">
 
+    <v-card flat class="block-card">
+
+    <!-- input -->
+      <v-card-text class="text-center">
+
+        <span class="text-blue"><h1><b>Your Portfolio Overview</b></h1></span><br>
+        
         To insert chart of user here
 
-        <div v-if="assets" style="text-align:left;">
-          Assets
-          <hr />
-          <table>
-            <tr>
-              <td></td>
-              <td>High</td>
-              <td>Low</td>
-              <td>Open</td>
-              <td>Close</td>
-            </tr>
-            <div v-for="(assets, index) in listofassets" :key="assets">
-              <tr>
-                <td>Assets {{ index + 1 }}</td>
-                <td v-for="value in assets" :key="value">
-                  {{ value }}
-                </td>
-              </tr>
-            </div>
-          </table>
+      <div v-if="assets" style="text-align:left;">
+        Assets
+        <hr>
+        <table>
+        <tr>
+          <td></td>
+          <td>High</td>
+          <td>Low</td>
+          <td>Open</td>
+          <td>Close</td>
+        </tr>
+        <div v-for="(assets,index) in listofassets" :key="assets">
+          <tr>
+          <td>Assets {{index+1}}</td>
+          <td v-for="value in assets" :key="value">
+            {{value}}
+          </td>
+          </tr>
         </div>
-      </v-card-text>
+        </table>
+      </div>
 
-      <div>
+      <!--Apex Chart--->
+        <div>
         <div class="chart-wrap">
           <div id="chart">
-            <apexchart
-              type="donut"
-              width="380"
-              :options="chartOptions"
-              :series="series"
-            ></apexchart>
-          </div>
+        <apexchart type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
+      </div>
         </div>
-
+      
         <div class="actions">
-          <button @click="appendData">
+          <button
+              
+              
+              @click="appendData">
             + ADD
           </button>
-
-          <button @click="removeData">
+          
+          <button
+              
+              
+              @click="removeData">
             - REMOVE
           </button>
-
-          <button @click="randomize">
+          
+          <button
+              
+              
+              @click="randomize">
             RANDOMIZE
           </button>
-
-          <button @click="reset">
+          
+          <button
+              
+              
+              @click="reset">
             RESET
           </button>
         </div>
       </div>
+      </v-card-text>
 
-      <!-- output -->
+    <!-- output --> 
+
     </v-card>
+
   </div>
 </template>
 
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
 import VueApexCharts from 'vue-apexcharts'
 
@@ -113,25 +125,10 @@ export default {
       arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1);
       this.series = arr;
     },
-
-    removeData: function() {
-      if (this.series.length === 1) return;
-      var arr = this.series.slice();
-      arr.pop();
-      this.series = arr;
-    },
-
-    randomize: function() {
-      this.series = this.series.map(function() {
-        return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-      });
-    },
-
-    reset: function() {
-      this.series = [44, 55, 13, 33];
+    mounted () {
     },
   },
-};
+}
 </script>
 
 <style scoped lang="scss">
