f022d98092e8ff72e148ddba48bd498acb3ebd5a
melkhun <melvin.ong.2017@sis.smu.edu.sg>
2020-09-07 12:06:22 +0800

    Resized donut & hardcoded it

--- frontend/src/components/chart-doughnut.vue
+++ frontend/src/components/chart-doughnut.vue
@@ -22,29 +22,46 @@
 
 <script>
 export default {
-  props: ['countEtf', 'countBond', 'countFuture', 'countOption', 'countEquity'],
+  props: ["countEtf", "countBond", "countFuture", "countOption", "countEquity"],
   data() {
     return {
       datasets: [
         {
           // data: [this.countEtf,this.countBond,this.countFuture,this.countOption,this.countEquity], //Think we need compute
-          data: [2,1,2,2,3], //Think we need compute
-          backgroundColor: ["#8544FA", "#493CDE", "#9FA8D", "#3C84DE", "#44C5FA"],
-          hoverBackgroundColor: ["#B47DFA", "#7567E0", "#C5CA", "#77ADE0", "#A2EAFA"]
-        }
+          data: [0, 0, 0, 0, 2], //Think we need compute
+          backgroundColor: [
+            "#8544FA",
+            "#493CDE",
+            "#9FA8D",
+            "#3C84DE",
+            "#44C5FA",
+          ],
+          hoverBackgroundColor: [
+            "#B47DFA",
+            "#7567E0",
+            "#C5CA",
+            "#77ADE0",
+            "#A2EAFA",
+          ],
+        },
       ],
       labels: ["ETFs", "Bonds", "Futures", "Options", "Equities"], //input instrument type as labels
-      option: {}
+      option: {},
     };
   },
   mounted() {
     this.renderChart(this.datasets, this.options);
-
   },
 };
 </script>
 
-
 <style scoped>
-
+.card-img-bottom {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  text-align: center;
+  max-width: 800px;
+}
 </style>
